0440c63021f77460fdecbfde210a65684057f45c
Qubit Security Inc. <plura@qubitsec.com>
2024-09-27 07:51:34 +0900

    Update tcp_error_daemon.sh

--- rocky9/net/debug/tcp_error_daemon.sh
+++ rocky9/net/debug/tcp_error_daemon.sh
@@ -8,9 +8,6 @@ LOGFILE="/root/tcpdump_timeouts.log"
 #tcpdump -i eth1 'tcp port 443' -nnvvv 2>&1 | grep -E 'tcp retransmission|dup ack' > $LOGFILE &
 tcpdump -i eth1 '(tcp port 443 or tcp port 80)' -nnvvv 2>&1 | grep -E 'tcp retransmission|dup ack' > $LOGFILE &
 
-#echo "Starting tcpdump..." >> /tmp/tcp_error_daemon_debug.log
-#tcpdump -i enp7s0 'tcp port 443' -nnvvv 2>&1 | grep -E 'tcp retransmission|dup ack' > $LOGFILE &
-
 # tcpdump  ID 
 TCPDUMP_PID=$!
 
